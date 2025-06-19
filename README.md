1d5ad33641f2ac02baf4f2170319a93b6f0effca
kaogilvie <kyle@kyleogilvie.com>
2025-06-19 13:38:50 -0700

    bumped setup.py version of snowflake conn

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # AMPERSAND DATASTORE
 &amp;KO upload interface to any arbitrary amount of datastores
 
+## Developing
+WHEN DEVELOPING, do NOT forget to change the versions in setup.py if you change them in requirements.txt!
+
 ### Python Version
 Supports Python 3.9 to 3.13 as of 6/19/2025.
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='ampersand_datastore',
       version=__version__,
       install_requires=[
         "psycopg2-binary==2.9.3",
-        "snowflake-connector-python==2.7.4",
+        "snowflake-connector-python==3.15.0",
         "requests==2.27.1",
         "google-cloud-bigquery==3.12.0"
       ]
